536811ec1a4aff288da194aef1314f2943ea167e
symax <maximsukhanov@gmail.com>
2014-05-06 19:05:36 +0700

    failed test3

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -60,10 +60,36 @@ namespace UnitTestProject1
             StreamWriter file = new StreamWriter("ideal.xml");
             xs.Serialize(file, idealList);
             file.Close();
+            //как сравнить 2 полученные xml?
+            //StreamReader idealSr = new StreamReader("ideal.xml");
+            //StreamReader testSr = new StreamReader("db.xml");
+            //Assert.AreEqual(idealSr, testSr);
+            ConcreteIterator ci = new ConcreteIterator(com);
+            Assert.AreEqual(true, ci.EqualSchedul(idealList));
+        }
 
-            StreamReader idealSr = new StreamReader("ideal.xml");
-            StreamReader testSr = new StreamReader("db.xml");
-            Assert.AreEqual(idealSr, testSr);
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<Schedul> idealList = new List<Schedul>();
+            Composite com = new Composite();
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe"));
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "Timer.exe"));
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "PatternCurs.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "Timer.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "PatternCurs.exe"));
+            com.Save();
+
+            FileStream fs = new FileStream("ideal.xml", FileMode.OpenOrCreate);
+            fs.Close();
+            XmlSerializer xs = new XmlSerializer(typeof(List<Schedul>));
+            StreamWriter file = new StreamWriter("ideal.xml");
+            xs.Serialize(file, idealList);
+            file.Close();
+
+            com = new Composite();
+
         }
     }
 }
