e228a236ee27fc952e21d183fed54d688ebbf362
symax <maximsukhanov@gmail.com>
2014-05-06 18:24:38 +0700

    new version

---
+++ UnitTestProject1/UnitTestProject1/Class1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Class1
+    {
+    }
+
+    public class Schedul
+    {
+            public DateTime date;
+            public string time;
+            public string file;
+            public bool isStarted = false;
+
+            public Schedul()
+            { }
+            public Schedul(DateTime d, string t, string f)
+            {
+                date = d;
+                time = t;
+                file = f;
+            }
+
+        public void start_process()
+        {
+            Timer _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            if (this.date.Date == DateTime.Now.Date)
+                if (this.time == DateTime.Now.ToString("HH:mm"))
+                    if (!this.isStarted)
+                    {
+                        System.Diagnostics.Process p = new System.Diagnostics.Process();
+                        p.StartInfo.FileName = this.file;
+                        p.Start();
+                        this.isStarted = true;
+                    }
+        }
+    }
+
+        public class Composite
+        {
+            List<Schedul> c = new List<Schedul>();
+
+            public void add(Schedul s)
+            {
+                c.Add(s);
+            }
+
+            public List<Schedul> getCompose()
+            {
+                return c;
+            }
+
+            public void addCompose(Composite com)
+            {
+                this.c.AddRange(com.c);
+            }
+        }
+
+        public interface Iterator
+        {
+            bool hasNext();
+            Schedul next();
+        }
+
+        public class ConcreteIterator : Iterator
+        {
+            List<Schedul> t = new List<Schedul>();
+
+            int index;
+
+            public ConcreteIterator(Composite c)
+            {
+                t = c.getCompose();
+                index = 0;
+            }
+
+            public ConcreteIterator(List<Schedul> l)
+            {
+                t = l;
+                index = 0;
+            }
+
+            public Schedul next()
+            {
+                index++;
+                return t[index - 1];
+            }
+
+            public void toFirst()
+            {
+                index = 0;
+            }
+
+            public bool hasNext()
+            {
+                if (t.Count > index)
+                    return true;
+                else
+                    return false;
+            }
+
+            public bool EqualSchedul(List<Schedul> SL)
+            {
+                bool IsEqual = true;
+                Schedul s = new Schedul();
+                foreach(Schedul sch in SL)
+                    if (this.hasNext())
+                    {
+                        s = this.next();
+                        if (s != sch)
+                        {
+                            IsEqual = false;
+                            break;
+                        }
+                    }
+                return IsEqual;
+            }
+        }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Timer myTimer = new Timer();
+            myTimer.Interval = 1000;
+            Schedul s = new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe");
+            Schedul ideal = new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe");
+            ideal.isStarted = true;
+            s.start_process();
+            foreach(System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                if(p.ProcessName.CompareTo("random.exe") == 0)
+                    Assert.AreEqual(s, ideal);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<Schedul> idealList = new List<Schedul>();
+            Composite com = new Composite();
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe"));
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "Timer.exe"));
+            com.add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "PatternCurs.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "random.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "Timer.exe"));
+            idealList.Add(new Schedul(DateTime.Now, DateTime.Now.ToString("HH:mm"), "PatternCurs.exe"));
+
+            ConcreteIterator ci = new ConcreteIterator(com);
+            Assert.AreEqual(true, ci.EqualSchedul(idealList));
+        }
+    }
+}
